9b8341afb512d97a834f0dc5172b06194e30dc61
Rodagus <prog.rodagus93@gmail.com>
2023-06-22 21:08:01 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Rodrigo Viveros Ramirez on 22-06-23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
